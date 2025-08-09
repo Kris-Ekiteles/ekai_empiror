e820460f53548c75789cda0604c0963e9a364d6c
kris <krisekteles@gmail.com>
2025-08-09 10:52:56 +0300

    card to be displayed

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Route, Router, Link } from 'react-router-dom'
+import "./Events.css"
+
 
 const Events = () => {
   return (
-  <h1>welcome to events</h1>
+  <div className='upcoming-events'>
+    <img src='' alt='' />
+<div className='event_description'>
+  <h2>name 1</h2>
+  <p className='event_disc'>trip to namanga</p>
+  <p className='price'>$20.0</p>
+  <button className='btn'>book slot</button>
+  </div> 
+  </div>
   );
 }
 
