5a1bdf9a34156948d99d1a655533695ce920bc98
kris <krisekteles@gmail.com>
2025-08-09 20:52:13 +0300

    img not complete. fix issue

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Route, Router, Link } from 'react-router-dom'
-import "./Events.css"
-
+import React from "react";
+import { Route, Router, Link } from "react-router-dom";
+import "./Events.css";
+import Image from "../Assets/namanga.png";
 
 const Events = () => {
   return (
-  <div className='upcoming-events'>
-    <img src='' alt='' />
-<div className='event_description'>
-  <h2>name 1</h2>
-  <p className='event_disc'>trip to namanga</p>
-  <p className='price'>$20.0</p>
-  <button className='btn'>book slot</button>
-  </div> 
-  </div>
+    <div className="upcoming-events">
+      <img src="" alt="" />
+      <div className="event_description">
+        <h2></h2>
+        <p className="event_disc"></p>
+        <p className="price"></p>
+        <button className="btn">book slot</button>
+      </div>
+    </div>
   );
-}
+};
 
-export default Events
+export default Events;
