aafbae0adc4f3b36aaf2ec335654c3e1be42d577
kris <krisekteles@gmail.com>
2025-08-30 08:03:11 +0300

    about us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import "./About.css"
 
 const About = () => {
   return (
-    <div>
-      <h2>this is the about section</h2>
+    <div className="About_us">
+      <div className="identity">
+        <img src="" alt="Ekai Safaris" />
+        <p>
+          {/* image should cover this seze or 3/4 of this size */}
+          Ekai Safaris is a premier safari and tour company dedicated to
+          crafting unforgettable journeys into the heart of Kenya's most
+          spectacular wilderness areas. We specialize in creating personalized,
+          immersive experiences that connect travelers with the raw beauty of
+          nature, from thrilling wildlife encounters in iconic national parks to
+          cultural interactions with local communities. With a deep commitment
+          to sustainable tourism, expert guides, and attention to detail, we
+          transform adventurous dreams into enduring memories, offering more
+          than just a trip—but a profound exploration of the wild.
+        </p>
+      </div>
+      <div className="about">
+        <h2>About EkaiSafaris</h2>
+        <p>
+          Ekai Safaris is a premier safari and tour company dedicated to
+          crafting unforgettable journeys into the heart of Kenya's most
+          spectacular wilderness areas. We specialize in creating personalized,
+          immersive experiences that connect travelers with the raw beauty of
+          nature, from thrilling wildlife encounters in iconic national parks to
+          cultural interactions with local communities. With a deep commitment
+          to sustainable tourism, expert guides, and attention to detail, we
+          transform adventurous dreams into enduring memories, offering more
+          than just a trip—but a profound exploration of the wild.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default About
