61464efb944736c5fed78ad51a89be912408ac5a
kris <krisekteles@gmail.com>
2025-08-30 18:00:15 +0300

    card to hold props for testimonials

---
+++ src/components/ReviewCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// shares css with about page
+import "./About.css";
+
+const ReviewCard = (props) => {
+  return (
+    <div className='review_card'>
+      <div className='review_message'>
+        <p>{props.text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewCard
