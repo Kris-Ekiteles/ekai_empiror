7897c209f79ab157c14b5e08e862455f7844c286
kris <krisekteles@gmail.com>
2025-08-31 17:06:58 +0300

    delete comment line leave room for image

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import "./About.css"
-import ReviewCard from './ReviewCard';
-import namanga from "../Assets/namanga.png"
+import React from "react";
+import "./About.css";
+import ReviewCard from "./ReviewCard";
+import namanga from "../Assets/namanga.png";
+// not the official item to be where placed
+import Card from "./Card";
 
 const About = () => {
   return (
@@ -9,32 +11,23 @@ const About = () => {
       <div className="About_us">
         <div className="identity">
           <img src={namanga} alt="Ekai Safaris" />
-          <p>
+          
             {/* image should cover this seze or 3/4 of this size */}
-            {/* Ekai Safaris is a premier safari and tour company dedicated to
-            crafting unforgettable journeys into the heart of Kenya's most
-            spectacular wilderness areas. We specialize in creating
-            personalized, immersive experiences that connect travelers with the
-            raw beauty of nature, from thrilling wildlife encounters in iconic
-            national parks to cultural interactions with local communities. With
-            a deep commitment to sustainable tourism, expert guides, and
-            attention to detail, we transform adventurous dreams into enduring
-            memories, offering more than just a trip—but a profound exploration
-            of the wild. */}
-          </p>
+         
         </div>
         <div className="about">
           <h2>About EkaiSafaris</h2>
+          
           <p>
             Ekai Safaris is a premier safari and tour company dedicated to
-            crafting unforgettable journeys into the heart of Kenya's most
-            spectacular wilderness areas. We specialize in creating
+            crafting unforgetable journeys into the hearts of kenya's most
+            spectacular wilderness areas. We specialise in creating
             personalized, immersive experiences that connect travelers with the
-            raw beauty of nature, from thrilling wildlife encounters in iconic
-            national parks to cultural interactions with local communities. With
-            a deep commitment to sustainable tourism, expert guides, and
+            raw beuty of nature, from thrilling wildlife encounters in iconic
+            national park to cultural interaction with local communities. with a
+            deep commitment to substainable tourism, expert guides, and
             attention to detail, we transform adventurous dreams into enduring
-            memories, offering more than just a trip—but a profound exploration
+            memories, offering more that just a trip but a profound exploration
             of the wild.
           </p>
         </div>
@@ -42,7 +35,7 @@ const About = () => {
       <div className="why_us">
         <h2> why us</h2>
         <div className="child safety">
-          {" "}
+      
           <h4>safety</h4>
           <p>
             Your security is our top priority. Our guides are trained in
@@ -65,6 +58,10 @@ const About = () => {
           </p>
         </div>
       </div>
+      <div className="join_us">
+        <h4>Join us As we expoler Kenya and have your best time</h4>
+        {/* the below card are temporarily here and shall be replaced later for the images cuptured in the recent adventures */}
+      </div>
       <div className="review">
         <h4>Testimonials</h4>
         <ReviewCard text="Nice experience" />
@@ -74,6 +71,6 @@ const About = () => {
       </div>
     </div>
   );
-}
+};
 
-export default About
+export default About;
