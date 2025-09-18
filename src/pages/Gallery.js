6f145c0e027bddbf597cc60a6de4aa112c2a0f2f
Kris-Ekiteles <krisekteles@gmail.com>
2025-09-18 22:52:57 +0300

    change in directory

---
+++ src/pages/Gallery.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Shop from './Shop'
+import './Shop.css'
+const Gallery = () => {
+  return (
+    <div>
+      <Shop
+        img=""
+        name="cap"
+        price="20"
+        action="order"
+      />
+    </div>
+  );
+}
+
+export default Gallery
