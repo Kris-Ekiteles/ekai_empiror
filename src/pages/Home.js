0c1496777caa66a8176d9ddb0e3dce7c498f7d06
Kris-Ekiteles <krisekteles@gmail.com>
2025-09-20 10:43:53 +0300

    remove h1 from hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ const Home = () => {
   return (
     <div>
       <div className='hero_section'>
- <h1>welcome to the home section</h1>
        <p><Typewriter words={['join us as we explore Kenya and have your best time', 'book with us for event planning', 'travel in style']} /></p>
       </div>
 <div className="hero-services">
