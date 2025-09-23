a60a6510afc4076058b92d6929f1ba617d2da241
Kris-Ekiteles <krisekteles@gmail.com>
2025-09-23 20:45:02 +0300

    added prompt card/component

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -4,24 +4,27 @@ import Image from "../Assets/namanga.png";
 import Card from "../components/Card";
 import suswa from "../Assets/suswa.jpg";
 import njiine from "../Assets/njiine_kabia.jpg";
+import PayNow from "../components/PayNow";
 
 const Events = () => {
   return (
     <div className="product">
       <Card
-        img={"suswa"}
+        img={suswa}
         name="suswa fun Drive"
         price="1700"
         button="book event"
         reserve="reserve slot"
       />
       <Card
-        img={"njiine"}
+        img={njiine}
         name="Njiine Kabia Expedition"
         price="3000"
         button="book event"
         reserve="reserve slot"
       />
+
+      <PayNow />
     </div>
   );
 };
