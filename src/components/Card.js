6abb856e39cfa44974aac6f977d7157781468aec
Kris-Ekiteles <krisekteles@gmail.com>
2025-09-23 20:42:19 +0300

    about styles changes/responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css"
+import PayNow from "./PayNow";
 
 const Card = (props) => {
+  const [showPay, setShowPay] = useState(false);
+
+  const openPay = () => setShowPay(true);
+  const closePay = () => setShowPay(false);
   return (
     <div className="upcoming-events">
       <img src={props.img} alt="" />
@@ -9,10 +14,11 @@ const Card = (props) => {
         <h3>{props.name}</h3>
         <p className="price">{props.price}</p>
         <div className="btn">
-          <button>{props.button}</button>
-          <button>{props.reserve}</button>
+          <button onClick={openPay}>{props.button}</button>
+          <button onClick={openPay}>{props.reserve}</button>
         </div>
       </div>
+      <PayNow isOpen={showPay} onClose={closePay} />
     </div>
   );
 };
